30c6de21dfd001bb0d1bc1fa88ecb2ef86f62f31
Meison <69295331+Meison12345@users.noreply.github.com>
2022-03-16 20:46:45 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const mysql = require('mysql2');
 app.use(express.json());
 /** Подключение nodemailer*/
 const nodemailer = require('nodemailer');
+/**Подключение SweetAlert2 */
+// const Swal = require('sweetalert2');
 
 let con = mysql.createConnection({
     host: 'localhost',
@@ -27,28 +29,36 @@ app.listen(port, () => {
     console.log(`node express work on ${port}`);
 });
 
-app.get('/', (req, res) => {
-    // console.log(req);
-    // let cat = new Promise(resolve,reject)
-    con.query("SELECT * FROM shop.goods", (error, result, field) => {
-        if (error) throw error;
-        // console.log(result);
-        // resolve(result);
-        let goods = {};
-        for (let i = 0; i < result.length; i++) {
-            goods[result[i]['id']] = result[i];
-        }
-        // console.log(goods);
-        console.log(JSON.parse(JSON.stringify(goods)));
+//Главная
+app.get('/', function(req, res) {
+    let cat = new Promise(function(resolve, reject) {
+        con.query(
+            "select id, name, cost, image, category from (select id,name,cost,image,category, if(if(@curr_category != category, @curr_category := category, '') != '', @k := 0, @k := @k + 1) as ind   from goods, ( select @curr_category := '' ) v ) goods where ind < 3",
+            function(error, result, field) {
+                if (error) return reject(error);
+                resolve(result);
+            }
+        );
+    });
+    let catDescription = new Promise(function(resolve, reject) {
+        con.query(
+            "SELECT * FROM category",
+            function(error, result, field) {
+                if (error) return reject(error);
+                resolve(result);
+            }
+        );
+    });
+    Promise.all([cat, catDescription]).then(function(value) {
+        console.log(value[1]);
         res.render('index', {
-            foo: 2,
-            bar: 'hello',
-            goods: JSON.parse(JSON.stringify(goods))
+            goods: JSON.parse(JSON.stringify(value[0])),
+            cat: JSON.parse(JSON.stringify(value[1])),
         });
     });
 });
 
-
+//Категории
 app.get('/cat', (req, res) => {
     let catId = req.query.id;
     let cat = new Promise((res, rej) => {
@@ -74,10 +84,10 @@ app.get('/cat', (req, res) => {
     });
 });
 
-
-app.get('/goods', function (req, res) {
-    console.log(req.query.id);
-    con.query('SELECT * FROM goods WHERE id=' + req.query.id, function (error, result, fiels) {
+//Товары
+app.get('/goods', function(req, res) {
+    // console.log(req.query.id);
+    con.query('SELECT * FROM goods WHERE id=' + req.query.id, function(error, result, fiels) {
         if (error) throw error;
         res.render('goods', {
             goods: JSON.parse(JSON.stringify(result)),
@@ -85,12 +95,96 @@ app.get('/goods', function (req, res) {
     });
 });
 
+//Корзина
+app.get('/order', function(req, res) {
+    res.render('order');
+});
 
-app.post('/get-category-list', function (req,res){
+
+
+app.post('/get-category-list', function(req, res) {
     // console.log(req);
-    con.query('SELECT id, category FROM category', function (error, result, fiels) {
+    con.query('SELECT id, category FROM category', function(error, result, fiels) {
         if (error) throw error;
-        console.log(result);
+        // console.log(result);
         res.json(result);
     });
 });
+
+
+app.post('/get-goods-info', function(req, res) {
+    // console.log(req.body.key);
+    if (req.body.key.length != 0) {
+        con.query('SELECT id, name, image, cost FROM shop.goods WHERE id IN(' + req.body.key.join(',') + ');', function(error, result, fiels) {
+            if (error) throw error;
+            console.log(result);
+            let goods = {};
+            for (let i = 0; i < result.length; i++) {
+                goods[result[i]['id']] = result[i];
+            }
+            res.json(goods);
+        });
+    } else {
+        res.send('0');
+    }
+});
+
+
+app.post('/finish-order', function(req, res) {
+    console.log(req.body);
+    if (req.body.key.length != 0) {
+        let key = Object.keys(req.body.key);
+        con.query(
+            'SELECT id,name,cost FROM goods WHERE id IN (' + key.join(',') + ')',
+            function(error, result, fields) {
+                if (error) throw error;
+                console.log(result);
+                sendMail(req.body, result).catch(console.error);
+                res.send('1');
+            });
+    } else {
+        res.send('0');
+    }
+});
+
+async function sendMail(data, result) {
+    let res = '<h2>Order in lite shop</h2>';
+    let total = 0;
+    let res_text = '';
+    for (let i = 0; i < result.length; i++) {
+        res += `<p>${result[i]['name']} - ${data.key[result[i]['id']]} - ${result[i]['cost'] * data.key[result[i]['id']]} ₽</p>`;
+        total += result[i]['cost'] * data.key[result[i]['id']];
+    }
+    console.log(res);
+    res += '<hr>';
+    res += `Итого ${total} ₽`;
+    res += `<hr>Телефон: ${data.phone}`;
+    res += `<hr>Имя: ${data.username}`;
+    res += `<hr>Адрес: ${data.address}`;
+    res += `<hr>Почта: ${data.email}`;
+
+    let testAccount = await nodemailer.createTestAccount();
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass // generated ethereal password
+        }
+    });
+
+    let mailOption = {
+        from: '<89606138203@mail.ru>',
+        to: "89606138203@mail.ru," + data.email,
+        subject: "Магазин ТехноЭра",
+        text: res_text,
+        html: res
+    };
+
+    let info = await transporter.sendMail(mailOption);
+    console.log("MessageSent: %s", info.messageId);
+    console.log("PreviewSent: %s", nodemailer.getTestMessageUrl(info));
+    return true;
+}
