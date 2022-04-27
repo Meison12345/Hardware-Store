2cacf69ec98f7e932840dd27172fee80ab932a71
Meison <69295331+Meison12345@users.noreply.github.com>
2022-04-27 07:00:52 +0300

    Add files via upload

--- public/js/order.js
+++ public/js/order.js
@@ -1,72 +1,74 @@
 'use strict';
-
-document.querySelector('#shop-order').addEventListener('submit', function(el) {
-    el.preventDefault();
-    let username = document.querySelector('#username').value.trim();
-    let phone = document.querySelector('#phone').value.trim();
-    let email = document.querySelector('#email').value.trim();
-    let address = document.querySelector('#address').value.trim();
-
-    if (!document.querySelector('#rule').checked) {
-        //С правилами не согласен
-        Swal.fire({
-            title: 'Предупреждение',
-            text: 'Примите правила',
-            // type: 'info',
-            icon: 'error',
-            confirmButtonText: 'ОК',
-        });
-        return false;
-    }
-    if (username == '' || phone == '' || email == '' || address == '') {
-        Swal.fire({
-            title: 'Предупреждение',
-            text: 'Заполните все поля',
-            // type: 'info',
-            icon: 'error',
-            confirmButtonText: 'ОК',
-        });
-    }
-
-    fetch('/finish-order', {
-            method: 'POST',
-            body: JSON.stringify({
-                'username': username,
-                'phone': phone,
-                'address': address,
-                'email': email,
-                'key': JSON.parse(localStorage.getItem('cart'))
-            }),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(function(response) {
-            return response.text();
-        })
-        .then(function(body) {
-            if (body == 1) {
-                Swal.fire({
-                    title: 'Всё хорошо',
-                    text: 'Удачно',
-                    type: 'info',
-                    // icon: 'error',
-                    confirmButtonText: 'ОК',
-                });
-            } else {
-                Swal.fire({
-                    title: 'Возникли проблемы',
-                    text: 'Ошибка',
-                    // type: 'info',
-                    icon: 'error',
-                    confirmButtonText: 'ОК',
-                });
-            }
-        })
-});
+try {
 
 
+    document.querySelector('#shop-order').addEventListener('submit', function (el) {
+        el.preventDefault();
+        let username = document.querySelector('#username').value.trim();
+        let phone = document.querySelector('#phone').value.trim();
+        let email = document.querySelector('#email').value.trim();
+        let address = document.querySelector('#address').value.trim();
+
+        if (!document.querySelector('#rule').checked) {
+            //С правилами не согласен
+            Swal.fire({
+                title: 'Предупреждение',
+                text: 'Примите правила',
+                // type: 'info',
+                icon: 'error',
+                confirmButtonText: 'ОК',
+            });
+            return false;
+        }
+        if (username == '' || phone == '' || email == '' || address == '') {
+            Swal.fire({
+                title: 'Предупреждение',
+                text: 'Заполните все поля',
+                // type: 'info',
+                icon: 'error',
+                confirmButtonText: 'ОК',
+            });
+        }
+
+        fetch('/finish-order', {
+                method: 'POST',
+                body: JSON.stringify({
+                    'username': username,
+                    'phone': phone,
+                    'address': address,
+                    'email': email,
+                    'key': JSON.parse(localStorage.getItem('cart'))
+                }),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (body) {
+                if (body == 1) {
+                    Swal.fire({
+                        title: 'Всё хорошо',
+                        text: 'Удачно',
+                        type: 'info',
+                        // icon: 'error',
+                        confirmButtonText: 'ОК',
+                    });
+                } else {
+                    Swal.fire({
+                        title: 'Возникли проблемы',
+                        text: 'Ошибка',
+                        // type: 'info',
+                        icon: 'error',
+                        confirmButtonText: 'ОК',
+                    });
+                }
+            })
+    });
+
+} catch (error) {}
 
 
 
